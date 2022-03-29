2e6922b92cc85f9efc2bec6413898ecc955a9158
unknown <rafaelhazevedo61@gmail.com>
2022-03-29 19:21:19 -0300

    commit

---
+++ src/br/com/padaria/util/Versionamento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.padaria.util;
+
+/**
+ *
+ * @author Rafael
+ */
+public class Versionamento {
+    
+    /*
+    V.1.0.1 - Rafael
+    1.Editados alguns detalhes na view ViewLogin
+    2.Inserido JPanelDesktop na view MenuPrincipal
+    3.Editados alguns detalhes na LoginController
+    4.Editados alguns detalhes na UsuarioDAO
+    */
+    
+}
